9839bb2d880d2dbcea94d50d16e0c3e4b1c9b015
ahabhgk <ahabhgk@gmail.com>
2019-06-01 09:11:49 +0800

    finish getNowData

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,21 @@
 import '../style/font/iconfont.css'
 import '../style/style.scss'
-import '../img/clear.jpg'
-import '../img/cloud.jpg'
-import '../img/overcast.jpg'
-import '../img/rain.jpg'
-import '../img/snow.jpg'
-import '../img/thunder.jpg'
+import clear from'../img/clear.jpg'
+import cloud from '../img/cloud.jpg'
+import overcast from '../img/overcast.jpg'
+import rain from '../img/rain.jpg'
+import snow from '../img/snow.jpg'
+import thunder from '../img/thunder.jpg'
 
 
+// 用于获取数据的
 async function getData(parameters) {
   const data = await fetch(`https://free-api.heweather.net/s6${parameters}&key=b52a498653264e23a6a795fb4ede5c97`)
   const json = await data.json()
   return json.HeWeather6[0]
 }
 
+// 用于搜索的
 async function getSearch(parameters) {
   const data = await fetch(`https://search.heweather.net${parameters}&key=b52a498653264e23a6a795fb4ede5c97`)
   const json = await data.json()
@@ -31,6 +33,72 @@ async function getHotCity() {
 }
 
 
+// 天气图标、背景和描述处理
+function getImgsByDesc(description) {
+  const set = {}
+  if (description.includes('晴')) {
+    set.bg = clear
+    set.iconClass = 'icon-clear'
+    set.desc = '今天天气不错呦～'
+  } else if (description.includes('阴')) {
+    set.bg = overcast
+    set.iconClass = 'icon-overcast'
+    set.desc = '天暗下来，你就是阳光～'
+  } else if (description.includes('雨')) {
+    set.bg = rain
+    set.iconClass = 'icon-rain'
+    set.desc = '下雨记得带伞哦～'
+  } else if (description.includes('多云')) {
+    set.bg = cloud
+    set.iconClass = 'icon-cloud'
+    set.desc = '现在温度比较凉爽～'
+  } else if (description.includes('雪')) {
+    set.bg = snow
+    set.iconClass = 'icon-snow'
+    set.desc = '来堆雪人吧～'
+  } else if (description.includes('雷')) {
+    set.bg = thunder
+    set.iconClass = 'icon-thunder'
+    set.desc = '小心装逼遭雷劈哦～'
+  }
+  return set
+}
+
+
+// 获取 now 的数据
+async function getNow(location) {
+  const data = await getData(`/weather/now?location=${location}`)
+  const airData = await getData(`/air/now?location=${location}`)
+  const set = getImgsByDesc(data.now.cond_txt)
+  return { location, ...data.now, ...airData.air_now_city, ...set }
+}
+
+// 展示 now 数据
+const nowTemp = document.querySelector('.now-temp')
+const nowWeather = document.querySelector('.now-weather')
+const humidityInfo = document.querySelector('.humidity-info')
+const windDir = document.querySelector('.wind-dir')
+const windSc = document.querySelector('.wind-sc')
+const airAqi = document.querySelector('.aqi')
+const airStatus = document.querySelector('.status')
+const nowDesc = document.querySelector('.now-desc')
+const now = document.querySelector('.now')
+const locationTit = document.querySelector('.location-tit')
+
+async function displayNow(data) {
+  locationTit.textContent = data.location
+  nowTemp.textContent = data.tmp
+  nowWeather.textContent = data.cond_txt
+  humidityInfo.textContent = data.hum
+  windDir.textContent = data.wind_dir
+  windSc.textContent = data.wind_sc
+  nowDesc.textContent = data.desc
+  now.style.backgroundImage = `url(${data.bg})`
+  airAqi.textContent = data.aqi
+  airStatus.textContent = data.qlty
+}
+
+
 // 页面加载好后的处理
 const historyInfos = document.querySelector('.history>.infos')
 
@@ -39,6 +107,14 @@ async function ready() {
   document.querySelector('.search-page').style.transition = 'all .6s'
   getHotCity()
   displayHistory()
+  let nowData
+  if (window.localStorage.cqData) {
+    nowData = JSON.parse(window.localStorage.cqData)
+  } else {
+    nowData = await getNow('重庆')
+    window.localStorage.cqData = JSON.stringify(nowData)
+  }
+  displayNow(nowData)
 }
 
 ready()
@@ -77,16 +153,6 @@ cancel.addEventListener('click', e => {
 
 
 // 搜索
-function debounce(fn, wait) { // 防抖
-  let timeout
-  return function (...arg) {
-    clearTimeout(timeout)
-    timeout = setTimeout(() => {
-      fn.apply(this, arg)
-    }, wait)
-  }
-}
-
 const search = document.querySelector('#search')
 const results = document.querySelector('.results')
 
@@ -125,8 +191,15 @@ async function displaySearchResult() {
   results.style.display = 'block'
 }
 
+// 展示结果
+async function displayResult(result) {
+  const data = await getNow(result)
+  displayNow(data)
+  cancel.click()
+}
+
 // 对搜索结果进行委托
-search.addEventListener('input', debounce(displaySearchResult, 400))
+search.addEventListener('change', displaySearchResult)
 searchPage.addEventListener('click', e => {
   if (e.target.classList.contains('result') || e.target.classList.contains('info')) {
     // 展示 hot 或搜索结果
